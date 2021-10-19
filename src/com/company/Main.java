46a0dd2078386b6e2c4f0b57bf8d7a1a90bcf4a7
tiko02220 <tiko02220@gmail.com>
2021-10-19 15:25:18 +0400

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,53 @@
 package com.company;
+
 public class Main {
     public static void main(String[] args) {
-Driver Driver1 = new Driver("Bob",20);
-Driver1.drive();
-Car car1 = new Car("BMW",1000);
-car1.start();
-car1.move();
-car1.stop();
+        Driver Driver1 = new Driver("Bob", 20);
+        Driver1.drive();
+        Car car1 = new Car("BMW", 1000);
+        car1.start();
+        car1.move();
+        car1.stop();
     }
 }
-class DriverProperties{
-    String name ;
-    int age ;
+
+class DriverProperties {
+    String name;
+    int age;
+
     public void drive() {
         System.out.println(name + " is Driving");
     }
 
 }
-class CarProperties{
-    String model ;
+
+class CarProperties {
+    String model;
     int price;
-    public void stop(){
+
+    public void stop() {
         System.out.println(model + " was stopped");
     }
-    public void start(){
+
+    public void start() {
         System.out.println(model + " started");
     }
-     public void move  (){
-    System.out.println(model + " is moving ");
+
+    public void move() {
+        System.out.println(model + " is moving ");
     }
 }
-class Car extends CarProperties{
-Car (String model ,int price){
-    this.model =model;
-    this.price = price;
+
+class Car extends CarProperties {
+    Car(String model, int price) {
+        this.model = model;
+        this.price = price;
     }
-  }
-class Driver extends DriverProperties{
-    Driver(String name, int age){
-        this.name =name; this.age= age;
+}
+
+class Driver extends DriverProperties {
+    Driver(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
